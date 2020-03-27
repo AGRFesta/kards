9debef2d9a7f5d85fd3c70297479469d958dd724
Alessandro Festa <agrfesta@gmail.com>
2020-03-27 17:22:01 +0100

    Card's equals and hashCode

--- playing-cards/src/main/kotlin/agrfesta/kcards/playingcards/cards/Cards.kt
+++ playing-cards/src/main/kotlin/agrfesta/kcards/playingcards/cards/Cards.kt
@@ -8,6 +8,11 @@ fun cardOf(rank: Rank, seed: Seed) = object: Card {
     override fun rank() = rank
     override fun seed() = seed
     override fun toString(): String = "${rank().symbol()}${seed().symbol()}"
+    override fun equals(other: Any?): Boolean {
+        if (other !is Card || other==null) return false
+        return rank()==other.rank() && seed()==other.seed()
+    }
+    override fun hashCode(): Int = seed().hashCode() * 31 + rank().hashCode()
 }
 
 interface Rank: Comparable<Rank> {
