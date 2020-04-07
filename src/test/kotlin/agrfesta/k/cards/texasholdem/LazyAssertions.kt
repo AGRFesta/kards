3f53851b1940b1940661fb2fd514f973de623b55
Alessandro Festa <agrfesta@gmail.com>
2020-04-07 11:51:27 +0200

    temporary comment unused notEqual lazy assertion

--- src/test/kotlin/agrfesta/k/cards/texasholdem/LazyAssertions.kt
+++ src/test/kotlin/agrfesta/k/cards/texasholdem/LazyAssertions.kt
@@ -4,7 +4,6 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isGreaterThan
 import assertk.assertions.isLessThan
-import assertk.assertions.isNotEqualTo
 import org.junit.jupiter.api.DynamicTest
 
 interface LazyAssertion {
@@ -18,10 +17,10 @@ class LazyAssertionEqual<T>(private val actual: T, private val other: T): LazyAs
     override fun assert() = assertThat(actual).isEqualTo(other)
     override fun toString() = "$actual == $other"
 }
-class LazyAssertionNotEqual<T>(private val actual: T, private val other: T): LazyAssertion {
-    override fun assert() = assertThat(actual).isNotEqualTo(other)
-    override fun toString() = "$actual != $other"
-}
+//class LazyAssertionNotEqual<T>(private val actual: T, private val other: T): LazyAssertion {
+//    override fun assert() = assertThat(actual).isNotEqualTo(other)
+//    override fun toString() = "$actual != $other"
+//}
 class LazyAssertionGreaterThan<A: Comparable<B>, B>(private val actual: A, private val other: B): LazyAssertion {
     override fun assert() = assertThat(actual).isGreaterThan(other)
     override fun toString() = "$actual > $other"
@@ -35,6 +34,6 @@ class LazyAssertionBuilder<T>(val actual: T)
 fun <A: Comparable<B>, B> LazyAssertionBuilder<A>.isGreaterThan(other: B): LazyAssertion = LazyAssertionGreaterThan(actual, other)
 fun <A: Comparable<B>, B> LazyAssertionBuilder<A>.isLessThan(other: B): LazyAssertion = LazyAssertionLessThan(actual, other)
 fun <T> LazyAssertionBuilder<T>.isEqualTo(other: T): LazyAssertion = LazyAssertionEqual(actual, other)
-fun <T> LazyAssertionBuilder<T>.isNotEqualTo(other: T): LazyAssertion = LazyAssertionNotEqual(actual, other)
+//fun <T> LazyAssertionBuilder<T>.isNotEqualTo(other: T): LazyAssertion = LazyAssertionNotEqual(actual, other)
 fun <T> willAssertThat(actual: T): LazyAssertionBuilder<T> = LazyAssertionBuilder(actual)
 
