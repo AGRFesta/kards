4523076cf4d14691d19453e2a3ccf2be84d10333
Alessandro Festa <agrfesta@gmail.com>
2020-04-10 00:57:59 +0200

    fix object mother with missing implementations

--- playing-cards/src/test/kotlin/agrfesta/kcards/playingcards/deck/ObjectMother.kt
+++ playing-cards/src/test/kotlin/agrfesta/kcards/playingcards/deck/ObjectMother.kt
@@ -5,6 +5,18 @@ import agrfesta.kcards.playingcards.cards.Seed
 
 fun rankOf(symbol: Char) = object: Rank {
     override fun symbol() = symbol
+    override fun ordinal(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun plus(increment: Int): Rank {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun minus(decrement: Int): Rank {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun compareTo(other: Rank): Int = -1
 }
 
