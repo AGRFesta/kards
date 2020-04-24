8db1bf166173479bc9bf4583474403e57e4fdb6a
Alessandro Festa <agrfesta@gmail.com>
2020-04-25 00:59:37 +0200

    remove game positions functions now included in table

--- src/main/kotlin/agrfesta/k/cards/texasholdem/rules/gameplay/Games.kt
+++
@@ -1,9 +0,0 @@
-package agrfesta.k.cards.texasholdem.rules.gameplay
-
-fun getSBPosition(btt: Int, players: Int): Int = if (players == 2) (btt%players + players)%players
-                                                    else ((btt%players + players)%players + 1) % players
-fun getBBPosition(btt: Int, players: Int): Int = (getSBPosition(btt, players) + 1) % players
-fun getUTGPosition(btt: Int, players: Int): Int = if (players == 2) getSBPosition(btt, players)
-                                                    else (getBBPosition(btt, players) + 1) % players
-
-
