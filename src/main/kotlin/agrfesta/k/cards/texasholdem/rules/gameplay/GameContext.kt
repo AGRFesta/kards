8c32cf93c0a2fe4db2400ed1dd4ce1ab69350e81
Alessandro Festa <agrfesta@gmail.com>
2020-04-25 01:14:08 +0200

    game context and payments

---
+++ src/main/kotlin/agrfesta/k/cards/texasholdem/rules/gameplay/GameContext.kt
@@ -0,0 +1,5 @@
+package agrfesta.k.cards.texasholdem.rules.gameplay
+
+class GameContext(val table: Table,
+                  val payments: GamePayments,
+                  val board: Board)
---
+++ src/main/kotlin/agrfesta/k/cards/texasholdem/rules/gameplay/GamePayments.kt
@@ -0,0 +1,15 @@
+package agrfesta.k.cards.texasholdem.rules.gameplay
+
+interface GamePayments {
+    fun sb(): Int
+    fun bb(): Int
+    fun ante(): Int?
+}
+
+class GamePaymentsFixedImpl(private val sb: Int, private val bb: Int, private val ante: Int?) : GamePayments {
+    constructor(sb: Int, bb: Int): this(sb,bb,null)
+
+    override fun sb(): Int = sb
+    override fun bb(): Int = bb
+    override fun ante(): Int? = ante
+}
