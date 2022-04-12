66e16bddf7cc458c9cb96ada59fc326c8fb51e48
Alessandro Festa <agrfesta@gmail.com>
2022-04-12 21:47:40 +0200

    DEPENDENCIES: upgraded k-playing-cards to version 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
 
-    api("org.agrfesta.k.kards:k-playing-cards:1.1.0-SNAPSHOT")
+    api("org.agrfesta.k.kards:k-playing-cards:1.1.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
